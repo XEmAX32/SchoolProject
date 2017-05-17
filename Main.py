3c7f2a377e1b934750da7f09b9be2bba7935c25b
XEmAX32 <XEmAX32@gmail.com>
2017-05-17 20:16:35 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,11 +2,39 @@ import RPi.GPIO as GPIO
 from time import sleep as sleep
 
 state = 1
-
-rpm = {1 : ,
-       2 : ,
-       3 : ,
-       }
+speed = 50
        
 GPIO.setmode(GPIO.BOARD)
 
+def set(property, val):
+        f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+        f.write(val)
+        f.close()
+
+set("deleyed, 0")
+set("mode, pwm")
+set("active, 1")
+set("frequency, 500")
+
+def worker:
+       if(state = 3):
+              global state = 1
+       else:
+              global state = state + 1
+              
+       if(state = 1):
+              global speed = 50
+       elif(state = 2):
+              global speed = 70
+       elif(state = 3):
+              global speed = 90
+       GPIO.output(motor_1, True)
+       GPIO.output(motor_1, False)
+       set("duty", str(speed))
+       #monitor(state)
+       sleep(60)
+       
+#def monitor:
+while True:
+       worker()
+       
